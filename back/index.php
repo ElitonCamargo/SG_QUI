b2964753f0743d48d783f9ed345a2531be48013e
Eliton Camargo <camargoliveira@gmail.com>
2024-04-02 10:00:02 -0300

    Config front

--- back/index.php
+++ back/index.php
@@ -6,5 +6,4 @@ header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 // header("Access-Control-Allow-Origin: $origin");
 // header("Access-Control-Allow-Headers: Content-Type");
 //*************************************************************************************
-$server = json_encode($_SERVER);
-echo $server;
+require_once('router/routers.php');
