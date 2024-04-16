0a5932eb5405ad0e146a6ed4a8b36aa8e0780439
Eliton Camargo <camargoliveira@gmail.com>
2024-04-16 19:52:11 -0300

    Tentando descobrir erro de conexão

--- back/app/Components/Elemento.php
+++ back/app/Components/Elemento.php
@@ -5,6 +5,7 @@ use app\database\Conexao;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use PDO;
+
 //Model ****************************************************************************************************************************************
     class Elemento{
         public $id;
--- back/index.php
+++ back/index.php
@@ -1,29 +1,38 @@
 <?php
+
+use app\database\Conexao;
+
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+require_once 'app/database/Conexao.php';
+
+$cx = new Conexao();
+
+var_dump($cx);
+
+// use Psr\Http\Message\ServerRequestInterface as Request;
+// use Slim\Factory\AppFactory;
 
 
-require __DIR__ . '/vendor/autoload.php';
-require_once 'app/helpers/funcoes.php';
-require_once 'app/helpers/autoload.php';
-$app = AppFactory::create();
-$app->setBasePath('/back'); // Define o caminho base do aplicativo
+// require __DIR__ . '/vendor/autoload.php';
+// require_once 'app/helpers/funcoes.php';
+// require_once 'app/helpers/autoload.php';
+// $app = AppFactory::create();
+// $app->setBasePath('/back'); // Define o caminho base do aplicativo
 
 
-$app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
-    $response = $handler->handle($request);
-    return $response->withHeader('Content-Type', 'application/json');
-});
+// $app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
+//     $response = $handler->handle($request);
+//     return $response->withHeader('Content-Type', 'application/json');
+// });
 
-require_once 'app/Components/Elemento.php';
-require_once 'app/Components/CompostoElemento.php';
-require_once 'app/Components/Composto_qui.php';
-require_once 'app/Components/MateriaPrima.php';
+// require_once 'app/Components/Elemento.php';
+// require_once 'app/Components/CompostoElemento.php';
+// require_once 'app/Components/Composto_qui.php';
+// require_once 'app/Components/MateriaPrima.php';
 
-require_once 'app/helpers/rota_nao_encontrada.php';
-$app->run();
+// require_once 'app/helpers/rota_nao_encontrada.php';
+// $app->run();
 
