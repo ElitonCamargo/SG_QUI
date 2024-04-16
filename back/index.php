9520acb16ded59102cf3b3588f01e4b91c9d0e76
Eliton Camargo <camargoliveira@gmail.com>
2024-04-16 18:30:18 -0300

    Configurando hostgator

--- back/index.php
+++ back/index.php
@@ -1,26 +1,28 @@
 <?php
-use Psr\Http\Message\ServerRequestInterface as Request;
-use Slim\Factory\AppFactory;
+// use Psr\Http\Message\ServerRequestInterface as Request;
+// use Slim\Factory\AppFactory;
 
 
-require __DIR__ . '/vendor/autoload.php';
-require_once 'App/helpers/funcoes.php';
-require_once 'App/helpers/autoload.php';
+// require __DIR__ . '/vendor/autoload.php';
+// require_once 'App/helpers/funcoes.php';
+// require_once 'App/helpers/autoload.php';
 
-$app = AppFactory::create();
-$app->setBasePath('/back'); // Define o caminho base do aplicativo
+// $app = AppFactory::create();
+// $app->setBasePath('/back'); // Define o caminho base do aplicativo
 
 
-$app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
-    $response = $handler->handle($request);
-    return $response->withHeader('Content-Type', 'application/json');
-});
+// $app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
+//     $response = $handler->handle($request);
+//     return $response->withHeader('Content-Type', 'application/json');
+// });
 
-require_once 'App/Components/Elemento.php';
-require_once 'App/Components/CompostoElemento.php';
-require_once 'App/Components/Composto_qui.php';
-require_once 'App/Components/MateriaPrima.php';
+// require_once 'App/Components/Elemento.php';
+// require_once 'App/Components/CompostoElemento.php';
+// require_once 'App/Components/Composto_qui.php';
+// require_once 'App/Components/MateriaPrima.php';
 
-require_once 'App/helpers/rota_nao_encontrada.php';
+// require_once 'App/helpers/rota_nao_encontrada.php';
 
-$app->run();
+// $app->run();
+
+echo 'ok';
