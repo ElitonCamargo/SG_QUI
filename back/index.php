f5c0a1aa7117b139a54e53eec08fdcc96e185b42
Eliton Camargo <camargoliveira@gmail.com>
2024-04-16 19:17:21 -0300

    Criando php.ini

--- back/index.php
+++ back/index.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 
@@ -6,21 +10,21 @@ use Slim\Factory\AppFactory;
 require __DIR__ . '/vendor/autoload.php';
 // require_once 'App/helpers/funcoes.php';
 // require_once 'App/helpers/autoload.php';
+include 'ffff.php';
+// $app = AppFactory::create();
+// $app->setBasePath('/back'); // Define o caminho base do aplicativo
 
-$app = AppFactory::create();
-$app->setBasePath('/back'); // Define o caminho base do aplicativo
 
-
-$app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
-    $response = $handler->handle($request);
-    return $response->withHeader('Content-Type', 'application/json');
-});
+// $app->add(function (Request $request, $handler) { // Define o middleware para transformar a resposta em JSON
+//     $response = $handler->handle($request);
+//     return $response->withHeader('Content-Type', 'application/json');
+// });
 
 // require_once 'App/Components/Elemento.php';
 // require_once 'App/Components/CompostoElemento.php';
 // require_once 'App/Components/Composto_qui.php';
 // require_once 'App/Components/MateriaPrima.php';
 
-require_once 'App/helpers/rota_nao_encontrada.php';
-echo 'fff';
-$app->run();
+// require_once 'App/helpers/rota_nao_encontrada.php';
+//$app->run();
+
