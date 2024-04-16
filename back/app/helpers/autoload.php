767cc7da88be30cca3762d6d98661264bb886962
Eliton Camargo <camargoliveira@gmail.com>
2024-04-16 20:27:09 -0300

    Tentando descobrir erro de conexão

--- back/app/helpers/autoload.php
+++ back/app/helpers/autoload.php
@@ -1,8 +1,8 @@
 <?php
 spl_autoload_register(function ($class) {
     $file = $class . '.php';
-    if (file_exists($file)) {
-        var_dump($file);
+    var_dump($file);
+    if (file_exists($file)) {        
         require_once $file;
         return;
     }
